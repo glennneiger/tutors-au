11c4942507021440067e7957bf760951885d0878
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-31 15:53:13 +0100

    update

--- aurelia_project/deploy-github.sh
+++ aurelia_project/deploy-github.sh
@@ -2,8 +2,8 @@
 au build --env dev
 cd ../wit-tutors.github.io
 git pull
-cd ../tutors-au
-cp ../tutors-au/* .
+cd ../tutors-au/
+cp ../tutors-au/dist/* .
 git add -A .
 git commit -m "update"
 git push
