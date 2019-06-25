c2ce4e28fc33a0a2ae0fa1ae1d4740bf4fd7c62b
Eamonn de Leastar <edeleastar@wit.ie>
2019-06-25 10:06:29 +0100

    enhanced mobile support

--- src/components/talk/talk-view.ts
+++ src/components/talk/talk-view.ts
@@ -27,15 +27,21 @@ export class TalkView {
   }
 
   refreshPdf() {
+
+    const fallback = `<br> <a class='uk-button uk-button-default' href='[url]'>Open ${this.lo.title} PDF`;
     var options = {
+
+
       pdfOpenParams: {
         navpanes: 1,
-        toolbar: 0,
+        toolbar: 1,
         statusbar: 1,
         // view: "FitV",
         pagemode: "thumbs",
-        page: 1
+        page: 1,
+          scrollbar: '1',
       },
+      fallbackLink: fallback,
       forcePDFJS: true
     };
     pdfobject.embed(this.lo.pdf, "#pdf-placeholder", options);
