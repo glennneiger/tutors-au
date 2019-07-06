b67238c1f326c46708d802740a60b92016b76298
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-06 08:35:28 +0100

    correct value bind filter

--- src/components/search/search-view.ts
+++ src/components/search/search-view.ts
@@ -3,7 +3,7 @@ import { NavigatorProperties } from "../../resources/elements/iconography/styles
 import { autoinject } from "aurelia-framework";
 import { Course } from "../../services/course";
 import environment from "../../environment";
-import {searchStrings} from "../../services/search-util";
+import { searchStrings } from "../../services/search-util";
 
 @autoinject
 export class SearchView {
@@ -15,14 +15,12 @@ export class SearchView {
   async activate(params) {
     this.course = await this.courseRepo.fetchCourse(params.courseurl);
 
-    this.navigatorProperties.title = this.course.lo.title
-    this.navigatorProperties.subtitle = 'Search...';
+    this.navigatorProperties.title = this.course.lo.title;
+    this.navigatorProperties.subtitle = "Search...";
     this.navigatorProperties.parentLink = `${environment.urlPrefix}/course/${this.courseRepo.courseUrl}`;
     this.navigatorProperties.parentIcon = "moduleHome";
     this.navigatorProperties.parentIconTip = "To module home ...";
 
     this.search_strings = searchStrings(this.course.lo.los);
-
-    // Todo: Search this.course.lo for search terms....
   }
 }
