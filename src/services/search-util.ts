4a85994b2d4545fb3bf94c3663d2e2ff46d34927
John Fitzgerald <usplitu@hotmail>
2019-07-07 20:10:42 +0100

    in progress: search component - labs only

--- src/services/search-util.ts
+++ src/services/search-util.ts
@@ -1,11 +1,11 @@
 import { Lo } from "./lo";
 
-function searchStrings(los: Lo[]) {
+function searchStrings(los: Lo[]) : string[] {
   let flatLos = flattenNestedLosArrays(los);
   let result: string[] = [];
   flatLos.forEach(lo => {
       console.log("lab ", lo);
-      result.push(`<a href="${lo.route}">" ${lo.shortTitle} </a>`);  
+      result.push(`<a href="${lo.route}"> ${lo.shortTitle}</a>`);  
   });
   return result;
 }
@@ -14,7 +14,7 @@ function flattenNestedLosArrays(los: Lo[]) {
   return flatten(los);
 }
 
-function flatten(arr, result = []) {
+function flatten(arr: Lo[], result = []) {
   for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
     if (Array.isArray(value.los)) {
@@ -27,26 +27,26 @@ function flatten(arr, result = []) {
 }
 
 
-export function filter(array, searchTerm) {
+function filter(array: string[], searchTerm: string) {
   return array.filter(item => {
       return searchTerm && searchTerm.length > 0 ? this.itemMatches(searchTerm, item) : true;
     });
 }
 
-  function itemMatches(searchTerm, value) {
-    let itemValue = value;
-    if (!itemValue) return false;
+function itemMatches(searchTerm: string, value: string) {
+  let itemValue = value;
+  if (!itemValue) return false;
     return itemValue.toUpperCase().indexOf(searchTerm.toUpperCase()) !== -1;
-  }
+}
 
-  export function search(los: Lo[], searchTerm: string) {
-    let search_strings: string[];
-    search_strings = searchStrings(los);
-    search_strings.filter(item => {
-      let found = searchTerm && searchTerm.length > 0 ? itemMatches(searchTerm, item) : true;
-      if(found) {
-        search_strings.push(item);
-      }
-    });
-    return search_strings;
-  }
+export function search(los: Lo[], searchTerm: string) {
+  let search_strings: string[] = [];
+  let all_strings = searchStrings(los);
+  all_strings.filter(item => {
+    let found = searchTerm && searchTerm.length > 0 ? itemMatches(searchTerm, item) : true;
+    if(found) {
+      search_strings.push(item);
+    }
+  });
+  return search_strings;
+}
