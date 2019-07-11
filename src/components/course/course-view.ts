fccff6f06ff11ac23cd1abc82bc507c7bcd20b8d
Eamonn de Leastar <edeleastar@wit.ie>
2019-07-11 08:00:32 +0100

    disable card support

--- src/components/course/course-view.ts
+++ src/components/course/course-view.ts
@@ -2,15 +2,23 @@ import { CourseRepo } from "../../services/course-repo";
 import { Course } from "../../services/course";
 import { NavigatorProperties } from "../../resources/elements/iconography/styles";
 import { autoinject } from "aurelia-framework";
-
+import { bindable } from "aurelia-framework";
 @autoinject
 export class CourseView {
   course: Course;
+
+  portfolio = false;
 
   constructor(private courseRepo: CourseRepo, private navigatorProperties: NavigatorProperties) {}
 
   async activate(params) {
     this.course = await this.courseRepo.fetchCourse(params.courseurl);
+    let p : any = this.course.lo.properties.portfolio
+    this.portfolio =  p as boolean;
     this.navigatorProperties.init(this.course);
+  }
+
+  determineActivationStrategy() {
+    return 'replace';
   }
 }
