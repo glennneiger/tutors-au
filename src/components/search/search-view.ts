a9415b11ebe29aa23e638df4384e8abc6f65ff61
John Fitzgerald <usplitu@hotmail>
2019-07-14 10:10:52 +0100

    housekeeping search-view.ts

--- src/components/search/search-view.ts
+++ src/components/search/search-view.ts
@@ -5,9 +5,6 @@ import { Course } from "../../services/course";
 import environment from "../../environment";
 import { flattenedLos } from "../../services/search-util";
 import { allLos } from "../../services/utils";
-import { Lo } from "../../services/lo";
-const path = require("path");
-import { MarkdownParser } from "../../services/markdown-parser";
 
 @autoinject
 export class SearchView {
@@ -15,7 +12,7 @@ export class SearchView {
   search_strings: string[] = [];
   searchTerm: string = "";
 
-  constructor(private courseRepo: CourseRepo, private navigatorProperties: NavigatorProperties, private markdownParser: MarkdownParser) {}
+  constructor(private courseRepo: CourseRepo, private navigatorProperties: NavigatorProperties) {}
 
   async activate(params) {
     this.course = await this.courseRepo.fetchCourse(params.courseurl);
@@ -26,38 +23,11 @@ export class SearchView {
     this.navigatorProperties.parentIcon = "moduleHome";
     this.navigatorProperties.parentIconTip = "To module home ...";
     this.setSearchStrings();
-    //this.currentChapter(params);
   }
 
   setSearchStrings() {
-    console.log("searchString changed: ", this.searchTerm);
-
     const labs = allLos("lab", this.course.lo.los);
-    //console.log(labs);
-
-    //this.search_strings = search(labs, this.searchTerm);
     this.search_strings = flattenedLos(labs, this.searchTerm);
-    //console.log("search strings", this.search_strings);
-  }
-
-  async currentChapter(params) {
-    const lastSegment = params.laburl.substr(params.laburl.lastIndexOf("/") + 1);
-    let chapter: Lo = null;
-    let currentChapter: Lo = null;
-    let url: string = "";
-    let lab: Lo = null;
-    if (lastSegment.startsWith("book")) {
-      url = params.laburl;
-      lab = await this.courseRepo.fetchLab(url);
-      console.log("lab retrieved");
-      currentChapter = lab.los[0];
-    } else {
-      url = path.dirname(params.laburl);
-      lab = await this.courseRepo.fetchLab(url);
-      currentChapter = lab.los.find(ch => ch.shortTitle == lastSegment);
-    }
-
-    let content = this.markdownParser.parse(currentChapter.contentMd, url);
   }
 
 }
