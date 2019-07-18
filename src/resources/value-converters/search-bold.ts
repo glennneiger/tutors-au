b76999339e46f8850fd9103727fefab0202445c2
John Fitzgerald <usplitu@hotmail>
2019-07-18 17:10:20 +0100

    housekeeping search bold value converter

--- src/resources/value-converters/search-bold.ts
+++ src/resources/value-converters/search-bold.ts
@@ -1,15 +1,11 @@
 export class SearchBoldValueConverter {
   toView(value, searchTerm) {
-    console.log(value);
-    if (!searchTerm) {
-      return value;
-    }
-    let separator = "/a>";
+    let separator = "</a>";
     let indx = value.indexOf(separator);
     let subs0 = value.substring(0, indx + separator.length); // the link (anchor) part of value.
-    let subs1 = value.substring(indx + separator.length, value.length); // the non link part of value.
-    let subs2 = subs1.replace(new RegExp(searchTerm, "gi"), `<b style="background-color:MediumSeaGreen;">$&</b>`); //make occurrences of searchTerm bold in non anchor part.
-    return subs0 + subs2; 
+    let subs1 = value.substring(indx + separator.length, value.length) // the non link part of value.
+                     .replace(new RegExp(searchTerm, "gi"), `<b style="background-color:MediumSeaGreen;">$&</b>`);
+    return subs0 + subs1; 
   }
 }
 
